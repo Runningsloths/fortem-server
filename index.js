f46c59f88c56f6282b942e9083bb277b1b48320c
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-07-25 00:34:31 -0700

    implemented test account and message creation

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
+let accounts = {
+  1: {
+    id: '1',
+    name: 'John',
+    password: 'pass',
+    email: 'rart@gmail.com',
+    jobTitle: 'physician',
+  }
+}
+
 let messages = {
   1: {
     id: '1',
@@ -13,16 +23,38 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-  const id = uuid();
+app.post('/message', (req, res) => {
+  const id = uuidv4();
   const message = {
     id: id,
     text: req.body.text,
   };
 
   messages[id] = message;
-  
   return res.send(message);
+})
+
+app.post('/account', (req, res) => {
+  const id = uuidv4();
+  for (const account of Object.values(accounts)) {
+    console.log(account);
+    if (req.body.email.toUpperCase() == account.email.toUpperCase()) {
+      return res.status(409).end("Account with this email already exists");
+    }
+    if (!req.body.email.includes('@') || !req.body.email.includes('.')) {
+      return res.status(400).end("Invalid email address");
+    }
+  }
+  const account = {
+    id: id,
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+    jobTitle: req.body.jobTitle,
+  };
+
+  accounts[id] = account;
+  return res.send(account);
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
