15d36263ae983fe0b9afb676c884515f45ed608d
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-07-25 21:19:52 -0700

    implemented accounts table

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const { OPEN_CREATE } = require('sqlite3');
 const sqlite3 = require('sqlite3').verbose();
 
 const DB_PATH = path.resolve(__dirname, "accounts.db");
@@ -14,7 +13,7 @@ let db = new sqlite3.Database(DB_PATH, (err) => {
 });
 
 db.serialize(() => {
-  db.run(`CREATE TABLE accounts (
+  db.run(`CREATE TABLE IF NOT EXISTS accounts (
     id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
     password TEXT NOT NULL,
@@ -24,12 +23,12 @@ db.serialize(() => {
     )`);
 });
 
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("close db connection");
-});
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log("close db connection");
+// });
 
 let accounts = {
   1: {
@@ -40,7 +39,7 @@ let accounts = {
     jobTitle: 'physician',
     phone: '8186969696'
   }
-}
+};
 
 let messages = {
   1: {
@@ -63,18 +62,17 @@ app.post('/message', (req, res) => {
 
   messages[id] = message;
   return res.send(message);
-})
+});
 
-app.post('/account', (req, res) => {
+app.post('/addAccount', (req, res) => {
   const id = uuidv4();
   for (const account of Object.values(accounts)) {
-    console.log(account);
-    if (req.body.email.toUpperCase() == account.email.toUpperCase()) {
+    if (req.body.email.toUpperCase() == account.email.toUpperCase())
       return res.status(409).end("Account with this email already exists");
-    }
-    if (!req.body.email.includes('@') || !req.body.email.includes('.')) {
-      return res.status(400).end("Invalid email address");
-    }
+    if (!req.body.email.includes('@') || !req.body.email.includes('.'))
+      return res.status(422).end("Invalid email address");
+    if (req.body.phone.length != 10)
+      return res.status(422).end("Invalid phone number");
   }
   const account = {
     id: id,
@@ -85,8 +83,12 @@ app.post('/account', (req, res) => {
     phone: req.body.phone,
   };
 
-  accounts[id] = account;
+  const stmt = db.prepare('INSERT INTO accounts (id, name, password, email, jobTitle, phone) VALUES (?, ?, ?, ?, ? ,?)');
+  console.log([ id, account.name, account.password, account.email, account.jobTitle, account.phone ]);
+  stmt.run(id, account.name, account.password, account.email, account.jobTitle, account.phone);
+  
   return res.send(account);
-})
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
